993c334eca64caa943d37f557496f33fe7ee850b
Mohamed Mohsen <Mohamed.Ebrahim.Mohsen@gmail.comgit config --global user.name MohamedEbrahimMohsen>
2018-04-17 02:57:23 +0200

    Starting With TableView ... Not Ended Yet

---
+++ ImageGallery/ImageGalleryCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  ImageGalleryCollectionViewCell.swift
+//  ImageGallery
+//
+//  Created by Mohamed Mohsen on 4/17/18.
+//  Copyright © 2018 Mohamed Mohsen. All rights reserved.
+//
+
+import UIKit
+
+class ImageGalleryCollectionViewCell: UICollectionViewCell {
+    
+    var image = UIImage()
+    
+}
